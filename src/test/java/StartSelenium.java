ebe1ae8dbf421f7f5abfed60586df7e4ae995010
MariaZH13 <134274880+MariaZH13@users.noreply.github.com>
2023-06-12 11:33:20 +0300

    HW1205

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -20,20 +20,26 @@ public class StartSelenium {
         //Old strategies
         wd.findElement(By.tagName("a"));
         wd.findElement(By.cssSelector("a"));
+        wd.findElement(By.xpath("//a"));
 
         wd.findElement(By.id("root"));
         wd.findElement(By.cssSelector("#root"));
+        wd.findElement(By.xpath("//*[@id='root']"));
 
         wd.findElement(By.className("container"));
         wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.xpath("//*[@class='container']"));
 
         wd.findElement(By.linkText("HOME"));
         wd.findElement(By.partialLinkText("OM"));
+        wd.findElement(By.xpath("//*[.='HOME']"));
 
         wd.findElement(By.cssSelector("[href='/login'"));
         wd.findElement(By.cssSelector("[href*='og'"));
         wd.findElement(By.cssSelector("[href^='/log'"));
         wd.findElement(By.cssSelector("[href$='in'"));
+        wd.findElement(By.xpath("//a[starts-with(@href,'/log')]"));
+        wd.findElement(By.xpath("//*[contains(@href,'/login')]"));
 
 
 
--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TableTest {
     WebDriver wd;
@@ -18,21 +19,33 @@ public class TableTest {
     public void init() {
         wd = new ChromeDriver();
         wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
+
     }
 
     @Test
     public void testCss(){
         wd.findElement(By.cssSelector("#customers tr:nth-child(7) td:last-child"));
 
+
         System.out.println(wd.findElements(By.cssSelector("#customers th")).size());
+        System.out.println(wd.findElements(By.xpath("//table[@class='ws-table-all']//th")).size());
+        System.out.println(wd.findElements(By.xpath("//*[@id='customers']//th")).size());
+
         System.out.println(wd.findElements(By.cssSelector("#customers tr")).size());
+        System.out.println(wd.findElements(By.xpath("//*[@id='customers']//tr")).size());
+        System.out.println(wd.findElements(By.xpath("//table[@class='ws-table-all']//tr")).size());
+
 
        // System.out.println(wd.findElements(By.cssSelector("#customers td:first-child")).size());
 
         WebElement row3= wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
         System.out.println(row3.getText());
-        WebElement lastColumn = wd.findElement(By.cssSelector("#customers td:last-child"));
-        System.out.println(lastColumn.getText());
+        wd.findElement(By.xpath("//*[@id='customers']//tr[position()=2]/child::td"));
+
+
+        wd.findElement(By.cssSelector("#customers td:last-child"));
+        wd.findElement(By.xpath("//*[@id='customers']//td[last()]"));
+
 
 
 
